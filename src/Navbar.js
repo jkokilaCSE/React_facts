5826730b318cc92a755dd1400fcd7f9f8b07ae66
jkokilaCSE <jkokila2810@gmail.com>
2023-07-24 22:38:03 +0530

    React facts

---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+
+
+export default function Nav(){
+    return(<nav>
+            <img src="logo192.png" alt="React logo" width="30px" height={"30px"} />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>)
+}
